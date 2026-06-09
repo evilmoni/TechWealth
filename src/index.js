c9aedbb4ac4d528ae567c6cc1ebcd1f3a23c6073
YBOT <ybotai@protonmail.com>
2026-06-09 05:30:59 +0000

    Fix Firebase config + rebuild

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDUDQti3a-SnstsakWSB6vTppsxDV_gh2Q",
   authDomain: "techwealth-website.firebaseapp.com",
   projectId: "techwealth-website",
-  storageBucket: "techwealth-website.firebasestorage.app",
+  storageBucket: "techwealth-website.appspot.com",
   messagingSenderId: "36453865287",
   appId: "1:36453865287:web:22bef340b02a7b8e385e62",
   measurementId: "G-HLRMJ3E11E"
