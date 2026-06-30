73970f478c63ccfd3e48a933a369ef5ec3db86fd
YBOT <ybotai@protonmail.com>
2026-06-30 13:15:49 +0000

    feat: Add GitHub Pages deployment workflow + static export config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com'],
   },
+  basePath: '',
+  assetPrefix: '',
 };
 
 module.exports = nextConfig;
